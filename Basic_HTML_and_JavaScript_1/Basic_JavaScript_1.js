7aac9f25f584e44592861912cc6da65317c247f9
Juvy0212 <168500164+Juvy0212@users.noreply.github.com>
2024-05-03 02:07:24 +0800

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+function My_First_Function(){
+		var string ="Kiss Me, I'm Irish";
+		var result = '<span style="color:green;">' +	string + '</span>';
+		document.getElementById("Irish").innerHTML=result;
+}
